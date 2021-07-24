fb310da4fc178b250bef02470de033cfb51c3797
uakarsh <55104596+uakarsh@users.noreply.github.com>
2021-07-24 20:39:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request, render_template
+from base64 import b64decode, b64encode
+from speech import prediction
+#from playsound import playsound
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if(request.method == 'POST'):
+        b64string = request.form['base64data']
+        decoded = b64decode(b64string)
+        with open("temp.wav", 'wb') as file:
+            file.write(decoded)
+        
+        #playsound('temp.wav')
+        return jsonify({
+            "The keyword in the audio is:": prediction('temp.wav'),
+        })
+
+
+if __name__ == "__main__":
+    app.run(port=3333, debug=True)
---
+++ speech.py
@@ -0,0 +1,23 @@
+from tensorflow import keras
+import numpy as np
+import librosa
+model = keras.models.load_model('speech-recognition.h5')
+target_list = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go', 'unknown','silence']
+
+
+def prediction(x):
+    sample,samples_rate = librosa.load(x)
+    samples = librosa.resample(sample,samples_rate,8000)
+    samples = np.expand_dims(samples,axis=0).reshape(-1,1)
+    query=np.expand_dims(samples,axis=0)
+    #print("The shape of samples is:",query.shape)
+    label = 'aeka'
+    label = target_list[np.argmax(model.predict(query))]
+    return label
+
+# file = 'audio/yes.wav'
+# sample,samples_rate = librosa.load(file)
+# samples = librosa.resample(sample,samples_rate,8000)
+# # samples = np.expand_dims(samples,axis=0).reshape(-1,1)
+# #print("The shape of the sample is:",samples.shape)
+# print("The keyword is:",prediction(file))
